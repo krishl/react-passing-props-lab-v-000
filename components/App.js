3556ee14928c468c81225cd92b5151778d20ede2
github username <krishel.lasam@gmail.com>
2017-06-06 18:21:11 -0400

    Done.

--- components/App.js
+++ components/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-
+import { Component } from 'react';
 import FruitBasket from './FruitBasket';
 
-const App = () => {
-  return (
-    <FruitBasket />
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fruit: [],
+      filters: [],
+      currentFilter: null
+    }
+    this.fetchFilters = this.fetchFilters.bind(this);
+  }
+
+  componentWillMount() {
+    this.fetchFilters();
+  }
+
+  fetchFilters() {
+    fetch('/api/fruit_types')
+      .then(res => res.json())
+      .then(filters => this.setState({filters: filters}));
+  }
+
+  render() {
+    return <div></div>
+  }
 }
 
 export default App;
--- components/FruitBasket.js
+++ components/FruitBasket.js
@@ -1,35 +1,22 @@
 import React from 'react';
-import { Component } from 'react';
-
 import Filter from './Filter';
 import FilteredFruitList from './FilteredFruitList.js';
 
-class FruitBasket extends Component {
-  constructor() {
-    super();
+const FruitBasket = (props) => {
+  return (
+    <div className="fruit-basket">
+      <Filter handleChange={props.handleFilterChange} />
+      <FilteredFruitList
+        filter={props.selectedFilter} />
+    </div>
+  );
+}
 
-    this.state = {
-      filters: [],
-      selectedFilter: null
-    };
-
-    this.handleFilterChange = this.handleFilterChange.bind(this);
-  }
-
-  handleFilterChange(e) {
-    console.log('new filter: ', e.target.value);
-    this.setState({ selectedFilter: e.target.value });
-  }
-
-  render() {
-    return (
-      <div className="fruit-basket">
-        <Filter handleChange={this.handleFilterChange} />
-        <FilteredFruitList
-          filter={this.state.selectedFilter} />
-      </div>
-    );
-  }
+FruitBasket.defaultProps = {
+  fruit: [],
+  filters: [],
+  currentFilter: null,
+  updateFilterCallback: function() {}
 }
 
 export default FruitBasket;
